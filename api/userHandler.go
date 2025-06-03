420e82650ce8ce9c51502c07589d9b66a44686f1
ivanSaichkin <ivan.saichkin@bk.ru>
2025-06-03 16:35:05 +0300

    add post handle

--- api/userHandler.go
+++ api/userHandler.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"context"
-
 	"github.com/GoDev/Hotel-reservatrion/db"
 	"github.com/GoDev/Hotel-reservatrion/types"
 
@@ -19,20 +17,39 @@ func NewUserHandler(userStore db.UserStore) *UserHandler {
 	}
 }
 
-func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
-	u := types.User{
-		FirstName: "James",
-		LastName:  "At the watercooler",
+func (h *UserHandler) HandlePostUser(c *fiber.Ctx) error {
+	var params types.CreateUserParams
+	if err := c.BodyParser(&params); err != nil {
+		return err
 	}
-	return c.JSON(u)
+
+	user, err := types.NewUserFromParams(params)
+	if err != nil {
+		return err
+	}
+
+	insertedUser, err := h.userStore.InsertUser(c.Context(), user)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(insertedUser)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
+	users, err := h.userStore.GetUsers(c.Context())
+	if err != nil {
+		return err
+	}
+	return c.JSON(users)
 }
 
 func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
 	var (
-		id  = c.Params("id")
-		ctx = context.Background()
+		id = c.Params("id")
 	)
-	user, err := h.userStore.GetUserByID(ctx, id)
+
+	user, err := h.userStore.GetUserByID(c.Context(), id)
 	if err != nil {
 		return err
 	}
--- go.mod
+++ go.mod
@@ -1,8 +1,14 @@
 module github.com/GoDev/Hotel-reservatrion
 
-go 1.22.2
+go 1.23.0
 
-require github.com/gofiber/fiber/v2 v2.52.8
+toolchain go1.24.3
+
+require (
+	github.com/gofiber/fiber/v2 v2.52.8
+	go.mongodb.org/mongo-driver v1.17.3
+	golang.org/x/crypto v0.38.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
@@ -21,9 +27,7 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	go.mongodb.org/mongo-driver v1.17.3 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 )
