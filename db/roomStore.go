4331749ed29efa18ae14e15bb8e261ef8e8f5832
ivanSaichkin <ivan.saichkin@bk.ru>
2025-06-06 13:11:34 +0300

    Add hotel api and add hotel seed script

--- db/roomStore.go
+++ db/roomStore.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/GoDev/Hotel-reservatrion/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -15,12 +16,15 @@ type RoomStore interface {
 type MongoRoomStore struct {
 	client *mongo.Client
 	coll   *mongo.Collection
+
+	HotelStore
 }
 
-func NemMongoRoomStore(client *mongo.Client, dbname string) *MongoRoomStore {
+func NemMongoRoomStore(client *mongo.Client, hotelStore HotelStore) *MongoRoomStore {
 	return &MongoRoomStore{
-		client: client,
-		coll:   client.Database(dbname).Collection("rooms"),
+		client:     client,
+		coll:       client.Database(DBNAME).Collection("rooms"),
+		HotelStore: hotelStore,
 	}
 }
 
@@ -32,8 +36,11 @@ func (s *MongoRoomStore) InsertRoom(ctx context.Context, room *types.Room) (*typ
 
 	room.ID = resp.InsertedID.(primitive.ObjectID)
 
-	// filter := bson.M{"_id": room.HotelID}
-	// update := bson.M{"$push": bson.M{"rooms": room.ID}}
-	// if err :=
+	filter := bson.M{"_id": room.HotelID}
+	update := bson.M{"$push": bson.M{"rooms": room.ID}}
+	if err = s.HotelStore.Update(ctx, filter, update); err != nil {
+		return nil, err
+	}
+
 	return room, nil
 }
--- scripts/seed.go
+++ scripts/seed.go
@@ -2,29 +2,28 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/GoDev/Hotel-reservatrion/db"
 	"github.com/GoDev/Hotel-reservatrion/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
-	ctx := context.Background()
+var (
+	client     *mongo.Client
+	roomStore  db.RoomStore
+	hotelStore db.HotelStore
+	ctx        = context.Background()
+)
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	hotelStore := db.NemMongoHotelStore(client, db.DBNAME)
-	roomStore := db.NemMongoRoomStore(client, db.DBNAME)
-
+func seedHotel(name string, location string, rating int) {
 	hotel := types.Hotel{
-		Name:     "Bellucia",
-		Location: "France",
+		Name:     name,
+		Location: location,
+		Rooms:    []primitive.ObjectID{},
+		Rating:   rating,
 	}
 
 	rooms := []types.Room{
@@ -48,10 +47,30 @@ func main() {
 	}
 	for _, room := range rooms {
 		room.HotelID = insertedHotel.ID
-		insertedRoom, err := roomStore.InsertRoom(ctx, &room)
+		_, err := roomStore.InsertRoom(ctx, &room)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(insertedRoom)
 	}
+}
+
+func main() {
+	seedHotel("Ballucia", "France", 3)
+	seedHotel("The cozy", "The Nederlands", 4)
+	seedHotel("Dont die in your sleep", "London", 1)
+}
+
+func init() {
+	var err error
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.Database(db.DBNAME).Drop(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	hotelStore = db.NemMongoHotelStore(client)
+	roomStore = db.NemMongoRoomStore(client, hotelStore)
 }
